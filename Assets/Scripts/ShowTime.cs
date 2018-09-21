b63de87e44efc3d7862c1f31b859ec5fd23c6cd4
zwz <zwz14@outlook.com>
2018-09-21 10:37:26 +0800

    add debug_text and depth_data_show

---
+++ Assets/Scripts/ShowTime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowTime : MonoBehaviour {
+
+    TextMesh textMesh;
+
+	// Use this for initialization
+	void Start () {
+        textMesh = gameObject.GetComponent<TextMesh>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int CurrentTime = (int)Time.time;
+        textMesh.text = CurrentTime.ToString();
+	}
+}
