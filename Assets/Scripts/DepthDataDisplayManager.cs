f7036bb500ce7ff4ef50576e7dcadaf97a6ff67a
zwz <zwz14@outlook.com>
2018-10-22 20:26:54 +0800

    add code about saving image in pgm in DepthDataDisplayManager

--- Assets/Scripts/DepthDataDisplayManager.cs
+++ Assets/Scripts/DepthDataDisplayManager.cs
@@ -23,6 +23,7 @@ public class DepthDataDisplayManager : MonoBehaviour
 
     // when index_num%10=0, save image
     private int index_num = 1;
+    private int index_num1 = 1;
 
     private Texture2D tex = null;
     private byte[] bytes = null;
@@ -59,9 +60,11 @@ public class DepthDataDisplayManager : MonoBehaviour
         
         var mediaFrameSourceGroupList = await MediaFrameSourceGroup.FindAllAsync();
         var mediaFrameSourceGroup = mediaFrameSourceGroupList[0];
-        var mediaFrameSourceInfo = mediaFrameSourceGroup.SourceInfos[0];
+        //var mediaFrameSourceInfo = mediaFrameSourceGroup.SourceInfos[0];
 
         Debug.Log("mediaFrameSourceGroup name:" + mediaFrameSourceGroup.DisplayName.ToString());
+
+        
 
         var mediaCapture = new MediaCapture();
         var settings = new MediaCaptureInitializationSettings()
@@ -71,13 +74,28 @@ public class DepthDataDisplayManager : MonoBehaviour
             StreamingCaptureMode = StreamingCaptureMode.Video,
             MemoryPreference = MediaCaptureMemoryPreference.Cpu,
         };
+
         try
         {
             await mediaCapture.InitializeAsync(settings);
+            /* --- test --- */
+            for(int i = 0; i < 2; i++)
+            {
+                var mediaFrameSourceInfo = mediaFrameSourceGroup.SourceInfos[i];
+                var mediaFrameSource = mediaCapture.FrameSources[mediaFrameSourceInfo.Id];
+                var mediaframereader = await mediaCapture.CreateFrameReaderAsync(mediaFrameSource, mediaFrameSource.CurrentFormat.Subtype);
+                Debug.Log("mediaFrameSource subtype" + i + " " + mediaFrameSource.CurrentFormat.Subtype.ToString());
+                mediaframereader.FrameArrived += FrameArrived;
+                await mediaframereader.StartAsync();
+            }
+            /*--------------*/
+
+            /*
             var mediaFrameSource = mediaCapture.FrameSources[mediaFrameSourceInfo.Id];
             var mediaframereader = await mediaCapture.CreateFrameReaderAsync(mediaFrameSource, mediaFrameSource.CurrentFormat.Subtype);
             mediaframereader.FrameArrived += FrameArrived;
             await mediaframereader.StartAsync();
+            */
         }
         catch (Exception e)
         {
@@ -94,9 +112,6 @@ public class DepthDataDisplayManager : MonoBehaviour
         {
             var videomediaframe = mediaframereference?.VideoMediaFrame;
             var softwarebitmap = videomediaframe?.SoftwareBitmap;
-
-            /*----------------*/
-
 
             /*
              * use pgmStorage() method to save bitmap image 
@@ -116,63 +131,116 @@ public class DepthDataDisplayManager : MonoBehaviour
                index_num++;
            }, true);
            */
-
-            if (softwarebitmap != null)
+            switch (mediaframereference.SourceKind.ToString())
             {
-                softwarebitmap = SoftwareBitmap.Convert(softwarebitmap, BitmapPixelFormat.Rgba8, BitmapAlphaMode.Premultiplied);
-                int w = softwarebitmap.PixelWidth;
-                int h = softwarebitmap.PixelHeight;
-                if (bytes == null)
-                {
-                    bytes = new byte[w * h * 4];
-                }
-                softwarebitmap.CopyToBuffer(bytes.AsBuffer());
-                softwarebitmap.Dispose();
-                UnityEngine.WSA.Application.InvokeOnAppThread(() => {
-                    if (tex == null)
+                case "Infrared":
+                    //Debug.Log("sourceKind is Infrared, save image");
+                    /**/
+                    UnityEngine.WSA.Application.InvokeOnAppThread(() =>
                     {
-                        tex = new Texture2D(w, h, TextureFormat.RGBA32, false);
-                        GetComponent<Renderer>().material.mainTexture = tex;
-                    }
-                    for (int i = 0; i < bytes.Length / 4; ++i)
-                    {
-                        byte b = bytes[i * 4];
-                        bytes[i * 4 + 0] = 0;
-                        bytes[i * 4 + 1] = 0;
-                        bytes[i * 4 + 2] = 0;
-                        bytes[i * 4 + 3] = 255;
-                        if (b == 0)
+
+                        //save image in local app data file
+                        if (index_num % 10 == 0 && index_num > 180 && index_num <= 210)
                         {
-                            bytes[i * 4 + 0] = 128;
+                            pgmStorage(softwarebitmap, "Infrared");
+                            Debug.Log("succeed in saving image" + "-index_num: Infrared" + index_num.ToString() + " " + Time.time.ToString());
                         }
-                        if (b == 1)
-                        {
-                            bytes[i * 4 + 0] = 255;
-                        }
-                    }
-                    tex.LoadRawTextureData(bytes);
+                        index_num++;
+                    }, true);
                     
-                    tex.Apply();
-                }, true);
-            }
-            mediaframereference.Dispose();
+                    break;
+                case "Depth":
+                    //Debug.Log("sourceKind is Depth, display image");
+                    /**/
+                    UnityEngine.WSA.Application.InvokeOnAppThread(() =>
+                    {
+
+                        //save image in local app data file
+                        if (index_num % 10 == 0 && index_num > 180 && index_num <= 210)
+                        {
+                            pgmStorage(softwarebitmap, "Depth");
+                            Debug.Log("succeed in saving image" + "-index_num: Depth" + index_num.ToString() + " " + Time.time.ToString());
+                        }
+                        index_num1++;
+                    }, true);
+                    /*
+                    if (softwarebitmap != null)
+                    {
+                        softwarebitmap = SoftwareBitmap.Convert(softwarebitmap, BitmapPixelFormat.Rgba8, BitmapAlphaMode.Premultiplied);
+                        int w = softwarebitmap.PixelWidth;
+                        int h = softwarebitmap.PixelHeight;
+                        if (bytes == null)
+                        {
+                            bytes = new byte[w * h * 4];
+                        }
+                        softwarebitmap.CopyToBuffer(bytes.AsBuffer());
+                        softwarebitmap.Dispose();
+                        UnityEngine.WSA.Application.InvokeOnAppThread(() => {
+                            if (tex == null)
+                            {
+                                tex = new Texture2D(w, h, TextureFormat.RGBA32, false);
+                                GetComponent<Renderer>().material.mainTexture = tex;
+                            }
+                            for (int i = 0; i < bytes.Length / 4; ++i)
+                            {
+                                byte b = bytes[i * 4];
+                                bytes[i * 4 + 0] = 0;
+                                bytes[i * 4 + 1] = 0;
+                                bytes[i * 4 + 2] = 0;
+                                bytes[i * 4 + 3] = 255;
+                                if (b == 0)
+                                {
+                                    bytes[i * 4 + 0] = 128;
+                                }
+                                if (b == 1)
+                                {
+                                    bytes[i * 4 + 0] = 255;
+                                }
+                            }
+                            tex.LoadRawTextureData(bytes);
+
+                            tex.Apply();
+                        }, true);
+                    }
+                    mediaframereference.Dispose();
+                    */
+                    break;
+            }         
         }
     }
 
-    private async void pgmStorage(SoftwareBitmap softwareBitmap)
+    private async void pgmStorage(SoftwareBitmap softwareBitmap, string sourceKind)
     {
         Debug.Log("Enter pgmStorage");
-        StorageFile outputFile = await FindTempStorageFileAsync_pgm();
+        StorageFile outputFile = await FindTempStorageFileAsync_pgm(sourceKind);
         // get pgm header
         int height = softwareBitmap.PixelHeight;
         int width = softwareBitmap.PixelWidth;
-        int maxValue = 65535;
+        int maxValue;
+        byte[] temp_bytes;
+        switch (sourceKind)
+        {
+            case "Infrared":
+                maxValue = 255;
+                temp_bytes = new byte[height * width];
+                break;
+            case "Depth":
+                maxValue = 65535;
+                temp_bytes = new byte[height * width * 2];
+                break;
+            default:
+                maxValue = 65535;
+                temp_bytes = new byte[height * width * 2];
+                break;
+        }      
         String pgmHeader = "P5\n" + width + " " + height + "\n" + maxValue + "\n";
         // get pgm raw data
-        byte[] temp_bytes = new byte[height * width * 2];
         softwareBitmap.CopyToBuffer(temp_bytes.AsBuffer());
         softwareBitmap.Dispose();
-        temp_bytes = process_bytes(temp_bytes);
+        if(sourceKind == "Depth")
+        {
+            temp_bytes = process_bytes(temp_bytes);
+        }
         // write image in pgm file
         var stream = await outputFile.OpenAsync(Windows.Storage.FileAccessMode.ReadWrite);
         using (var outputStream = stream.GetOutputStreamAt(0))
@@ -200,12 +268,12 @@ public class DepthDataDisplayManager : MonoBehaviour
         return bytes;
     }
 
-    private async Task<StorageFile> FindTempStorageFileAsync_pgm()
+    private async Task<StorageFile> FindTempStorageFileAsync_pgm(string sourceKind)
     {
         Debug.Log("Enter FindTempStorageFileAsync");
         StorageFile outputFile = null;
         StorageFolder temperoryStorageFolder = ApplicationData.Current.TemporaryFolder;
-        String outputFileName = Time.time.ToString() + ".pgm";
+        String outputFileName = Time.time.ToString() + sourceKind + ".pgm";
         outputFile = await temperoryStorageFolder.CreateFileAsync(outputFileName, CreationCollisionOption.GenerateUniqueName);
         return outputFile;
     }
