33bef2966dbb25c4fde467804304540097bc0a26
zwz <zwz14@outlook.com>
2018-12-05 19:20:00 +0800

    remove invalid code and update README

--- Assets/Scripts/DepthDataDisplayManager.cs
+++ Assets/Scripts/DepthDataDisplayManager.cs
@@ -20,15 +20,12 @@ public class DepthDataDisplayManager : MonoBehaviour
     private bool startMedia = false;
     private float StartTime;
     private float spatialMappingLastTime = new PlaySpaceManager().scanTime;
-
     // when index_num%10=0, save image
-    private bool showCameraIntrinsic = false;
     private int index_num = 1;
     private int index_num1 = 1;
-
     private Texture2D tex = null;
     private byte[] bytes = null;
-    // Use this for initialization
+
     void Start()
     {
         StartTime = Time.time;
@@ -61,11 +58,8 @@ public class DepthDataDisplayManager : MonoBehaviour
         
         var mediaFrameSourceGroupList = await MediaFrameSourceGroup.FindAllAsync();
         var mediaFrameSourceGroup = mediaFrameSourceGroupList[0];
-        //var mediaFrameSourceInfo = mediaFrameSourceGroup.SourceInfos[0];
 
         Debug.Log("mediaFrameSourceGroup name:" + mediaFrameSourceGroup.DisplayName.ToString());
-
-        
 
         var mediaCapture = new MediaCapture();
         var settings = new MediaCaptureInitializationSettings()
@@ -79,24 +73,15 @@ public class DepthDataDisplayManager : MonoBehaviour
         try
         {
             await mediaCapture.InitializeAsync(settings);
-            /* --- test --- */
             for(int i = 0; i < 2; i++)
             {
                 var mediaFrameSourceInfo = mediaFrameSourceGroup.SourceInfos[i];
                 var mediaFrameSource = mediaCapture.FrameSources[mediaFrameSourceInfo.Id];
                 var mediaframereader = await mediaCapture.CreateFrameReaderAsync(mediaFrameSource, mediaFrameSource.CurrentFormat.Subtype);
-                Debug.Log("mediaFrameSource subtype" + i + " " + mediaFrameSource.CurrentFormat.Subtype.ToString());
+                //Debug.Log("mediaFrameSource subtype" + i + " " + mediaFrameSource.CurrentFormat.Subtype.ToString());
                 mediaframereader.FrameArrived += FrameArrived;
                 await mediaframereader.StartAsync();
             }
-            /*--------------*/
-
-            /*
-            var mediaFrameSource = mediaCapture.FrameSources[mediaFrameSourceInfo.Id];
-            var mediaframereader = await mediaCapture.CreateFrameReaderAsync(mediaFrameSource, mediaFrameSource.CurrentFormat.Subtype);
-            mediaframereader.FrameArrived += FrameArrived;
-            await mediaframereader.StartAsync();
-            */
         }
         catch (Exception e)
         {
@@ -112,35 +97,12 @@ public class DepthDataDisplayManager : MonoBehaviour
         if (mediaframereference != null)
         {
             var videomediaframe = mediaframereference?.VideoMediaFrame;
-            var softwarebitmap = videomediaframe?.SoftwareBitmap;
-            /**/
-            UnityEngine.WSA.Application.InvokeOnAppThread(() =>
-            {
-                try
-                {
-                    Guid MFSampleExtension_SensorStreaming_CameraIntrinsics;
-                    Guid.TryParse("5dcc7829-9471-4d78-8c27-5b2b9a0ec5ef", out MFSampleExtension_SensorStreaming_CameraIntrinsics);
-                    System.Object value;
-                    mediaframereference.Properties.TryGetValue(MFSampleExtension_SensorStreaming_CameraIntrinsics, out value);
-                    
-                    if (!showCameraIntrinsic)
-                    {
-                        Debug.Log("value tye: " + value.GetType().ToString());
-                        showCameraIntrinsic = true;
-                    }
-                }
-                catch (Exception e)
-                {
-                    Debug.Log(e.ToString());
-                }
-                
-            }, true);
+            var softwarebitmap = videomediaframe?.SoftwareBitmap;           
             switch (mediaframereference.SourceKind.ToString())
             {
                 case "Infrared":
-                    //Debug.Log("sourceKind is Infrared, save image");
                     /*
-                     * use pgmStorage() method to save bitmap image 
+                     * use pgmStorage() method to save depth image 
                      * in TableKeyboard/localAppData/Template, this method
                      * should be called in main thread
                      */
@@ -156,10 +118,14 @@ public class DepthDataDisplayManager : MonoBehaviour
                         }
                         index_num++;
                     }, true);
-                    */
+                    */                   
                     break;
                 case "Depth":
-                    //Debug.Log("sourceKind is Depth, display image");
+                    /*
+                     * use pgmStorage() method to save IR image 
+                     * in TableKeyboard/localAppData/Template, this method
+                     * should be called in main thread
+                     */
                     /*
                     UnityEngine.WSA.Application.InvokeOnAppThread(() =>
                     {
@@ -173,7 +139,6 @@ public class DepthDataDisplayManager : MonoBehaviour
                         index_num1++;
                     }, true);
                     */
-                    /**/
                     if (softwarebitmap != null)
                     {
                         softwarebitmap = SoftwareBitmap.Convert(softwarebitmap, BitmapPixelFormat.Rgba8, BitmapAlphaMode.Premultiplied);
@@ -212,10 +177,11 @@ public class DepthDataDisplayManager : MonoBehaviour
                             tex.Apply();
                         }, true);
                     }
-                    mediaframereference.Dispose();
                     
+                    //mediaframereference.Dispose();
                     break;
-            }         
+            }
+            
         }
     }
 
@@ -265,19 +231,7 @@ public class DepthDataDisplayManager : MonoBehaviour
         }
         stream.Dispose(); 
     }
-
-    private byte[] process_bytes(byte[] bytes)
-    {
-        int length = bytes.Length;
-        for(int i = 0; i < length/2; i++)
-        {
-            byte temp = bytes[2 * i + 1];
-            bytes[2 * i + 1] = bytes[2 * i];
-            bytes[2 * i] = temp;
-        }
-        return bytes;
-    }
-
+    
     private async Task<StorageFile> FindTempStorageFileAsync_pgm(string sourceKind)
     {
         Debug.Log("Enter FindTempStorageFileAsync");
@@ -287,5 +241,19 @@ public class DepthDataDisplayManager : MonoBehaviour
         outputFile = await temperoryStorageFolder.CreateFileAsync(outputFileName, CreationCollisionOption.GenerateUniqueName);
         return outputFile;
     }
+
+    // exchange high 8 bits with low 8 bits
+    private byte[] process_bytes(byte[] bytes)
+    {
+        int length = bytes.Length;
+        for (int i = 0; i < length / 2; i++)
+        {
+            byte temp = bytes[2 * i + 1];
+            bytes[2 * i + 1] = bytes[2 * i];
+            bytes[2 * i] = temp;
+        }
+        return bytes;
+    }
+
 #endif
 }
--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 TableKeyBoard APP in C#
+
+---------- Purpose ----------
+
+final purpose: develop AR keyboard and recognize typing word using depth camera image
+
+current: get depth image and IR image from depth camera and simply process
+
+---------- version ----------
+
+Unity 2017.4.8f1
+
+Visual Studio 2017
+
+---------- procedure ----------
+
+run /App on Hololens, look around to scan environment, wait for a while, then plane will show depth image, type keyboard to move, type again to put keyboard on surface in your environment.
+
+
+---------- Assets/Scripts file ----------
+
+DepthDataDisplayManager.cs: get depth and IR image, display on a plane Object and store image in Hololens
+
+DebugWindow.cs: show debug message on 3D Text Object, used for debugging
+
+ReadBytesFile.cs: load z_compensate.bytes from Assets/Resource
+
+
+---------- other materials ----------
+
+1) how to start developing on Hololens
+
+   official guide: https://docs.microsoft.com/zh-cn/windows/mixed-reality/academy
+
+   MixedRealityToolkit: https://github.com/Microsoft/MixedRealityToolkit-Unity
+
+2) how to get data from depth camera from Hololens
+    
+   official project: https://github.com/Microsoft/HoloLensForCV
+
+   foundations:
+
+   	Media Foundation: https://docs.microsoft.com/zh-cn/windows/desktop/medfound/media-foundation-programming-guide
+
+   	Hololens Research Mode: https://docs.microsoft.com/en-us/windows/mixed-reality/research-mode
+
+   github project: https://github.com/akihiro0105/HoloLensResearchmodeDemoWithUnity
+
+   				   http://akihiro-document.azurewebsites.net/post/hololens_researchmode2/
+
+3) why change 8 high bits with low 8 bits 
+
+   refer to https://github.com/Microsoft/HoloLensForCV/issues/19 answer by 'jiying61306'
+
+4) where the depth and IR image store in
+
+	access Hololens with Windows Device Portal(use IP), in System/File explorer/LocalAppData/TableKeyBoard/TempState
+
+5) where I get keyboard model
+
+   I model keyboard in Solidworks and convert format to .FBX in 3DMax.
